19f8a49c79f5e82e5f801ba63389d8acec10e2f6
marouaseder-805 <marouaseder@gmail.com>
2025-12-19 22:59:25 +0100

    Update Reseau.py

--- Reseau.py
+++ Reseau.py
@@ -16,9 +16,6 @@ class Reseau:
         else:
             self.noeud_entree = -1
 
-    def ajouter_noeud(self, n: int, coords: tuple[int, int]):
-        if n >= 0:
-            self.noeuds[n] = coords
 
     def ajouter_arc(self, n1: int, n2: int) -> None:
         if n1 > n2:
@@ -34,19 +31,85 @@ class Reseau:
         self.strat = strat
 
     def valider_reseau(self) -> bool:
-        # TODO
-        return False
+        if self.noeud_entree == -1:
+            return False
+
+        visites = set()
+        a_faire = [self.noeud_entree]
+
+        while a_faire:
+            n = a_faire.pop()
+            if n not in visites:
+                visites.add(n)
+                for (a, b) in self.arcs:
+                    if a == n and b not in visites:
+                        a_faire.append(b)
+                    elif b == n and a not in visites:
+                     a_faire.append(a)
+
+        return len(visites) == len(self.noeuds)
+    def ajouter_noeud(self, n: int, coords: tuple[int, int]):
+        if n >= 0:
+            self.noeuds[n] = coords
+
 
     def valider_distribution(self, t: Terrain) -> bool:
-        # TODO
-        return False
+    # Le réseau doit déjà être valide
+        if not self.valider_reseau():
+            return False
+
+    # Parcours du réseau depuis le noeud d'entrée
+        visites = set()
+        pile = [self.noeud_entree]
+
+        while pile:
+            n = pile.pop()
+            if n not in visites:
+                visites.add(n)
+                for a, b in self.arcs:
+                    if a == n and b not in visites:
+                        pile.append(b)
+                    elif b == n and a not in visites:
+                        pile.append(a)
+
+    # Vérifier que chaque client est sur un noeud atteignable
+        for li in range(len(t.cases)):
+            for co in range(len(t.cases[li])):
+                if t.cases[li][co] == Case.CLIENT:
+                    trouve = False
+                    for nid, (nx, ny) in self.noeuds.items():
+                        if nid in visites and nx == li and ny == co:
+                            trouve = True
+                            break
+                    if not trouve:
+                        return False
+
+        return True
+
+
+
+
+
+
+
+
 
     def configurer(self, t: Terrain):
         self.noeud_entree, self.noeuds, self.arcs  = self.strat.configurer(t)
 
     def afficher(self) -> None:
-        # TODO
-        pass
+        for ligne in self.cases:
+            for case in ligne:
+                if case == Case.VIDE:
+                    print("~", end="")
+                elif case == Case.CLIENT:
+                    print("C", end="")
+                elif case == Case.ENTREE:
+                    print("E", end="")
+                elif case == Case.OBSTACLE:
+                    print("X", end="")
+            print()
+
 
     def afficher_avec_terrain(self, t: Terrain) -> None:
         for ligne, l in enumerate(t.cases):
@@ -85,4 +148,3 @@ class Reseau:
             else:
                 cout += 1
         return cout
-
